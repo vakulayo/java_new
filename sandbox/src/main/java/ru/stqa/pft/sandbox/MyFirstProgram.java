57f467fcd63918079ec74008893f97e029754ae3
vakulayo <vakula.yo@gmail.com>
2017-06-29 18:08:06 +0300

    add Point class and distance function  - homework1 (1,2,3)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.javaws.jnl.ResourcesDesc;
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
@@ -17,13 +19,26 @@ public class MyFirstProgram {
 
         System.out.println("площадь прямоугольника со сторонами  "+rectangle.a+" и " +rectangle.b+"  = "+rectangle.area());
 
+        Point a = new Point();
+        Point b = new Point();
 
+        a.x=0;
+        a.y=2;
+
+        b.x=3;
+        b.y=3;
+      System.out.println("расстояние между точками ("  +  a.x +   "," + a.y +") и ( "+  b.x +   "," + b.y +") равно " + distance(a,b));
     }
 
     public static void hello(String s){
         System.out.println("Hello, " + s +"!");
     }
 
+    public static double distance(Point a, Point b){
+      double dist;
+      dist = Math.sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
+      return dist;
+    }
 
 
 
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by test on 29.06.2017.
+ */
+public class Point {
+  public double x;
+  public double y;
+}
