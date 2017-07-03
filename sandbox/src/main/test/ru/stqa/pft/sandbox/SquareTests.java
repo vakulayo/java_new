c56988341fe13c78ed8b68788ea2a641b8076a8b
vakulayo <vakula.yo@gmail.com>
2017-07-03 14:40:10 +0300

    add several tests for Point class

---
+++ sandbox/src/main/test/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.Square;
+
+/**
+ * Created by test on 30.06.2017.
+ */
+public class SquareTests {
+
+  @Test
+  public void testArea(){
+    Square square = new Square(5);
+    Assert.assertEquals(square.area(),25.0);
+
+  }
+}
