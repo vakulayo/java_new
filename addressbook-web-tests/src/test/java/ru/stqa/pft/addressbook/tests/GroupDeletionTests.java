8b32228badd1df0c47929917d56dd6c185e390e4
vakulayo <vakula.yo@gmail.com>
2017-08-08 17:44:24 +0300

    check pre-condition for Group Deletion/Modification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.addressbook.tests;
 
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
@@ -10,6 +11,10 @@ public class GroupDeletionTests extends TestBase {
   public void testGroupDeletion() {
 
     app.getNavigationHelper().gotoGroupPage();
+
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
