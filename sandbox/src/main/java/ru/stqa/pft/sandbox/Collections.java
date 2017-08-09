eb42682e40ce8d2246b2bc11551a03210ea9ab88
vakulayo <vakula.yo@gmail.com>
2017-08-09 17:55:46 +0300

    try array and list construction

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,35 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by test on 09.08.2017.
+ */
+public class Collections {
+  public static void main(String[] args){
+    String[] langs = {"Java",    "C#", "Phyton",    "PHP"};
+
+
+    List<String> languages = Arrays.asList("Java",    "C#", "Phyton",    "PHP");
+
+   /* languages.add("Java");
+    languages.add("C#");
+    languages.add("Phyton");
+    languages.add("PHP");
+
+
+    for(int i=0;i<langs.length;i++){
+     System.out.println("Я хочу выучить "+langs[i]);
+   }*/
+
+    for(int i=0;i< languages.size();i++){
+      System.out.println("Я хочу выучить "+languages.get(i));
+    }
+
+    for(String l : languages){
+      System.out.println("Я хочу выучить "+l);
+    }
+  }
+}
