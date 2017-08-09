52035083e5c5f9e64056272425f4d3d82f6048bd
vakulayo <vakula.yo@gmail.com>
2017-08-09 17:12:17 +0300

    try for and while construction

---
+++ sandbox/src/main/test/ru/stqa/pft/sandbox/PrimeTests.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by test on 09.08.2017.
+ */
+public class PrimeTests {
+
+  @Test
+  public  void TestPrime() {
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+
+  @Test
+  public  void TestNonPrime() {
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE -2 ));
+  }
+
+  @Test (enabled = false)
+  public  void TestPrimeLong() {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+}
