7c9eb107e7b441c3ee8a202f905795678172b7bd
vakulayo <vakula.yo@gmail.com>
2017-08-11 18:31:29 +0300

    add lambda function for comparator

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+
 import java.util.HashSet;
 import java.util.List;
 
@@ -22,10 +23,7 @@ public class GroupCreationTests extends TestBase {
     Assert.assertEquals(after.size(),before.size() + 1);
 
 
-    int max = 0;
-    for(GroupData g : after){
-      if (g.getId()>max) {max = g.getId();}
-    }
+    int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId();
     groupNew.setId(max);
     before.add(groupNew);
 
