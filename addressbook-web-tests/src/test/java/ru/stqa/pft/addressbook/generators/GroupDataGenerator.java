fd790eac4ddda1b60cd824fe015e31cd7dee3b03
vakulayo <vakula.yo@gmail.com>
2017-09-05 16:12:46 +0300

    Analyse command line using jCommander library

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,9 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import org.junit.runners.Parameterized;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -13,28 +17,46 @@ import java.util.List;
  * Created by test on 04.09.2017.
  */
 public class GroupDataGenerator {
+
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
   public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
-    List<GroupData> groups = generateGroups(count);
-    save(groups,file);
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+
+
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
-     Writer writer = new FileWriter(file);
-    for(GroupData gd : groups){
-      writer.write(String.format("%s;%s;%s\n",gd.getName(),gd.getHeader(),gd.getFooter()));
+  private void run() throws IOException {
+    List<GroupData> groups = generateGroups(count);
+    save(groups, new File(file));
+  }
+
+  private void save(List<GroupData> groups, File file) throws IOException {
+    Writer writer = new FileWriter(file);
+    for (GroupData gd : groups) {
+      writer.write(String.format("%s;%s;%s\n", gd.getName(), gd.getHeader(), gd.getFooter()));
     }
     writer.close();
   }
 
 
-
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
 
     List<GroupData> groups = new ArrayList<GroupData>();
-    for(int i = 0 ; i < count ; i++){
-        groups.add(new GroupData().withName(String.format("test %s",i)).withHeader(String.format("header %s",i)).withFooter(String.format("footer %s",i)));
+    for (int i = 0; i < count; i++) {
+      groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
     }
     return groups;
 
