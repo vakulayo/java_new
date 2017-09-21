4467ca7e8ca2c1c03f870790343fd03b34c256ff
vakulayo <vakula.yo@gmail.com>
2017-09-21 15:52:40 +0300

    Make direct connection with DataBase and extract data from here

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import java.sql.*;
+import java.util.TimeZone;
+
+/**
+ * Created by test on 19.09.2017.
+ */
+public class DbConnectionTest {
+
+  @Test
+  public void testDbConnection(){
+    Connection conn = null;
+
+    try {
+   
+      conn =DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+      Statement st = conn.createStatement();
+      ResultSet resultSet = st.executeQuery("select group_id, group_name, group_header, group_footer  from group_list");
+
+      Groups groups = new Groups();
+      while (resultSet.next()){
+        groups.add(new GroupData().withId(resultSet.getInt("group_id")).withName(resultSet.getString("group_name"))
+                .withHeader(resultSet.getString("group_header")).withFooter(resultSet.getString("group_footer")));
+      }
+      resultSet.close();
+      st.close();
+      conn.close();
+
+      System.out.println(groups);
+
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+  }
+}
+
+
