80e6af5c9d5135842bb5815f667cfe8e9ad60018
vakulayo <vakula.yo@gmail.com>
2017-11-13 19:59:17 +0300

    correct config for mantis

--- mantis-tests/build/resources/test/config_inc.php
+++ mantis-tests/build/resources/test/config_inc.php
@@ -1,7 +1,12 @@
 <?php
 $g_hostname = 'localhost';
-$g_db_type = 'mysql';
+$g_db_type = 'mysqli';
 $g_database_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
 $g_signup_use_captcha = OFF;
+
+$g_default_timezone       = 'Europe/Moscow';
+
+
+$g_crypto_master_salt     = '4B1BwBex3JF+wz62LDobfa8lRkRgBmDTRWkSeJSncMo=';
