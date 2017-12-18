23d4cfe69a3797c0dd46cf197099bc73535dd458
vakulayo <vakula.yo@gmail.com>
2017-12-18 22:41:44 +0300

    changes in configuration

---
+++ out/test/mantis-tests/config_inc.php
@@ -0,0 +1,14 @@
+<?php
+$g_hostname = 'localhost';
+$g_db_type = 'mysqli';
+$g_database_name = 'bugtracker';
+$g_db_username = 'root';
+$g_db_password = '';
+$g_signup_use_captcha = OFF;
+
+$g_default_timezone       = 'Europe/Moscow';
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
+
+
+$g_crypto_master_salt     = '4B1BwBex3JF+wz62LDobfa8lRkRgBmDTRWkSeJSncMo=';
