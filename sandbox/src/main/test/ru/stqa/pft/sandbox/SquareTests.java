329abeef67f4075afc08e369b80680254370b84b
vakulayo <vakula.yo@gmail.com>
2017-12-18 18:51:33 +0300

    change tests for testing jenkins

--- sandbox/src/main/test/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/main/test/ru/stqa/pft/sandbox/SquareTests.java
@@ -12,7 +12,7 @@ public class SquareTests {
   @Test
   public void testArea(){
     Square square = new Square(5);
-    Assert.assertEquals(square.area(),25.0);
+    Assert.assertEquals(square.area(),26.0);
 
   }
 }
