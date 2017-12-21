01ab3afcab739e9e30eb9fdb2d38c4841e95a856
vakulayo <vakula.yo@gmail.com>
2017-12-21 22:17:58 +0300

    Oportunity to start browser remotely

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,16 +1,20 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -43,15 +47,24 @@ public class ApplicationManager {
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
 
     dbHelper = new DbHelper();
-    if (browser.equals(BrowserType.FIREFOX)) {
-      System.setProperty("webdriver.gecko.driver", "c:\\SeleniumGecko\\geckodriver-v0.17.0-win32\\geckodriver.exe");
-      wd = new FirefoxDriver();
 
+    if ("".equals(properties.getProperty("selenium.server"))){
+
+      if (browser.equals(BrowserType.FIREFOX)) {
+        System.setProperty("webdriver.gecko.driver", "c:\\SeleniumGecko\\geckodriver-v0.17.0-win32\\geckodriver.exe");
+        wd = new FirefoxDriver();
+
+      }
+      else if (browser.equals(BrowserType.IE)) {wd = new InternetExplorerDriver();}
+      else if (browser.equals(BrowserType.CHROME)) {wd = new ChromeDriver();}
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
-    else if (browser.equals(BrowserType.IE)) {wd = new InternetExplorerDriver();}
-    else if (browser.equals(BrowserType.CHROME)) {wd = new ChromeDriver();}
 
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+
+    wd.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);
 
    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
